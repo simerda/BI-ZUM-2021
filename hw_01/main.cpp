13a610c7255b7566d26895e2e74c8a2b7915843f
Jan Šimerda <simerda.jan@gmail.com>
2021-03-11 11:31:18 +0100

    nfc: Adding algo selection.

--- hw_01/main.cpp
+++ hw_01/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Maze.h"
+#include "SearchAlgorithmInterface.h"
 #include "RandomSearch.h"
 #include "DepthFirstSearch.h"
 #include "BreathFirstSearch.h"
@@ -17,10 +18,34 @@ int main()
         cout << "Wrong input!" << endl;
         return 1;
     }
-
     Maze maze(path);
-    AStarSearch searchAlgorithm;
-    maze.solve(searchAlgorithm);
 
+    cout << "Choose algorithm for path finding:\n\n1) Random search\n2) DFS\n3) BFS\n4) Greedy search\n5) A*" << endl;
+
+    int selection = -1;
+    while (!(cin >> selection) || selection <= 0 || selection > 5){
+        cin.clear();
+        cin.ignore(INT32_MAX, '\n');
+
+        cout << "Invalid selection, choose again." << endl;
+    }
+    cin.ignore(INT32_MAX, '\n');
+
+    SearchAlgorithmInterface* searchAlgorithm;
+    switch (selection) {
+        case 1: searchAlgorithm = new RandomSearch;
+            break;
+        case 2: searchAlgorithm = new DepthFirstSearch;
+            break;
+        case 3: searchAlgorithm = new BreathFirstSearch;
+            break;
+        case 4: searchAlgorithm = new GreedySearch;
+            break;
+        case 5: searchAlgorithm = new AStarSearch;
+            break;
+    }
+    maze.solve(*searchAlgorithm);
+
+    delete searchAlgorithm;
     return 0;
 }
